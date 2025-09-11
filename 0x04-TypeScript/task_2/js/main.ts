9f9452998bc4bbfd8c8378a5e931a589b14927bd
jungles001 <olusojiisraelife@gmail.com>
2025-09-11 22:19:12 +0100

    Advanced types Part 1

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -35,7 +35,7 @@ class Teacher implements TeacherInterface {
   }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) return new Teacher();
+function createEmployee(salary: number | ): Director | Teacher {
+  if ( salary < 500) return new Teacher();
   return new Director();
 }
