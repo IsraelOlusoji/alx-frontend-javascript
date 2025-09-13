33d78e734a595db5d29550adbb5ca97d1a4988bc
jungles001 <olusojiisraelife@gmail.com>
2025-09-13 23:24:57 +0100

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -11,5 +11,5 @@ const updateRow: RowElement = { ...row, age: 23 };
 
 const updatedRow: RowElement = { ...row, age: 23 };
 
-CRUD.updateRow(newRowID, updateRow);
+CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
